644a612837774f42857c4f455f82a73d8036b9fa
Meezaan Davids <meezaandavids365@gmail.com>
2026-06-04 14:28:03 +0200

    Clean up stale project artifacts

--- supabase/migrations/002_owner_read_policy.sql
+++
@@ -1,6 +0,0 @@
-drop policy if exists "Members can read watchlists" on public.watchlists;
-drop policy if exists "Owners and members can read watchlists" on public.watchlists;
-
-create policy "Owners and members can read watchlists"
-on public.watchlists for select
-using (owner_id = auth.uid() or public.is_watchlist_member(id));
